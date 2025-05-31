a601b0a3b567518b8f5412cfc3099834e86cab56
Robert Finch <5563401+robfinch@users.noreply.github.com>
2025-05-31 01:56:01 -0400

    Create mpmc11_od_ack.sv

---
+++ mpmc11/mpmc11_od_ack.sv
@@ -0,0 +1,69 @@
+`timescale 1ns / 1ps
+// ============================================================================
+//        __
+//   \\__/ o\    (C) 2025  Robert Finch, Waterloo
+//    \  __ /    All rights reserved.
+//     \/_//     robfinch<remove>@finitron.ca
+//       ||
+//
+// BSD 3-Clause License
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//
+// Generate read ack, crossing clock domains.
+// The ack pulse should be active for only a single clock cycle of the
+// destination clock domain.
+// ============================================================================
+//
+
+module mpmc11_od_ack(rst, clk, port, fifo_port, port_clk, rdy, port_ack);
+input rst;
+input clk;
+input [3:0] port;
+input [3:0] fifo_port;
+input port_clk;
+input rdy;
+output port_ack;
+
+reg chclkd;
+reg chod_ack;
+reg chod_acks;
+always_ff @(posedge clk) chclkd <= port_clk;
+
+always_ff @(posedge clk)
+if (rst)
+	chod_ack <= 1'd0;
+else begin
+	if (port_clk & ~chclkd) chod_ack <= 1'b0;
+	if (rdy && port==fifo_port)
+		chod_ack <= 1'b1;
+end
+// Synchronize to port's clock domain.
+always_ff @(posedge port_clk) chod_acks <= chod_ack;
+
+assign port_ack = chod_acks;
+
+endmodule
